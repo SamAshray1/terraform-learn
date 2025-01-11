ec61bf0dd69236bc45fa9a569fc6971d71e29c77
Sam Ashray <sam.ashray1@gmail.com>
2025-01-11 17:59:20 +0530

    added security groups to allow ssh by ansible

--- project/main.tf
+++ project/main.tf
@@ -12,14 +12,15 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "ansible" {
-  key_name = "ansible-key"
+  key_name   = "ansible-key"
   public_key = file("~/.ssh/aws.pub")
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.ansible.key_name  
+  ami             = "ami-011899242bb902164"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.ansible.key_name
+  security_groups = [aws_security_group.instances.name]
 
   tags = {
     Name = "WebServer"
@@ -28,6 +29,20 @@ resource "aws_instance" "example" {
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > inventory.txt"
   }
+}
+
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 output "public_ip" {
